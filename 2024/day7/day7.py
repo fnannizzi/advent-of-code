b9c45e37303b91ea59607714e95a86729f883a3c
Francesca Nannizzi <francesca.nannizzi@kensho.com>
2024-12-09 14:55:08 -0500

    day 7 part 2

--- 2024/day7/day7.py
+++ 2024/day7/day7.py
@@ -6,41 +6,48 @@ import itertools
 class Operator(Enum):
   ADDITION = 1
   MULTIPLICATION = 2
+  CONCATENATION = 3
 
-def generate_all_operator_combinations(num_operators: int):
+def generate_all_operator_combinations(num_operators: int, with_concat=False):
   # n-choose-k with repetition to get all possible operator combinations
   operator_types = [Operator.MULTIPLICATION, Operator.ADDITION]
+  if with_concat:
+    operator_types.append(Operator.CONCATENATION)
   return [p for p in itertools.product(operator_types, repeat=num_operators)]
+
+def concat_nums(left: int, right: int):
+  return int(str(left) + str(right))
+
 
 def calculate_result_from_nums_and_operators(nums: [], operators: []):
   result = nums[0]
-
   for i in range(len(nums[1:])):
     if operators[i] == Operator.ADDITION:
       result += nums[i + 1]
     elif operators[i] == Operator.MULTIPLICATION:
       result *= nums[i + 1]
+    elif operators[i] == Operator.CONCATENATION:
+      result = concat_nums(result, nums[i + 1])
     else:
       assert "Invalid operator type:" + operators[i]
-
   return result
 
 
-def eqn_is_valid(nums: []):
+def eqn_is_valid(nums: [], with_concat=False):
   result = nums[0]
   # for n numbers, there will be n-1 operators
   num_operators = len(nums[1:]) - 1
-  operator_combos = generate_all_operator_combinations(num_operators)
+  operator_combos = generate_all_operator_combinations(num_operators, with_concat)
 
   for combo in operator_combos:
     if calculate_result_from_nums_and_operators(nums[1:], combo) == result:
       return True
   return False
 
-def part_one(eqns):
+def get_sum_of_valid_eqns(eqns, with_concat=False):
   sum = 0
   for eqn in eqns:
-    if eqn_is_valid(eqn):
+    if eqn_is_valid(eqn, with_concat):
       sum += eqn[0]
   return sum
 
@@ -52,6 +59,5 @@ def parse_input(input):
 
 if __name__ == '__main__':
   eqns = parse_input(read_input('input.txt'))
-  print("sum of valid equations:" + str(part_one(eqns)))
-  # part_two(map)
-
+  print("sum of valid equations: " + str(get_sum_of_valid_eqns(eqns, with_concat=False)))
+  print("sum of valid equations with concatenation: " + str(get_sum_of_valid_eqns(eqns, with_concat=True)))
--- 2024/day7/test.py
+++ 2024/day7/test.py
@@ -5,4 +5,12 @@ class Test(unittest.TestCase):
   eqns = parse_input(read_input('test_input.txt'))
 
   def test_part_one(self):
-    self.assertEqual(part_one(self.eqns), 3749)
+    self.assertEqual(get_sum_of_valid_eqns(self.eqns, with_concat=False), 3749)
+
+  def test_eqn_is_valid_with_concat(self):
+    eqn = [7290, 6, 8, 6, 15]
+    operators = [Operator.MULTIPLICATION, Operator.CONCATENATION, Operator.MULTIPLICATION]
+    self.assertEqual(calculate_result_from_nums_and_operators(eqn[1:], operators), 7290)
+    self.assertEqual(eqn_is_valid(eqn, with_concat=True), True)
+  def test_part_two(self):
+    self.assertEqual(get_sum_of_valid_eqns(self.eqns, with_concat=True), 11387)
